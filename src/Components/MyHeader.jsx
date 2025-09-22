eeb83e577d1edfd0eb82649ed6beaf8c152911b1
Mohammed Sameh <ms122g@gmail.com>
2025-09-22 23:47:12 +0300

    fix The Nav

--- src/Components/MyHeader.jsx
+++ src/Components/MyHeader.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useAuthStore } from "../Store";
 import Logo from "../assets/store-2017.svg";
+
 export default function MyHeader() {
   const [isOpen, setIsOpen] = useState(false);
   const { logout } = useAuthStore();
   const navigate = useNavigate();
+
   const handleLogout = () => {
     logout();
     navigate("/LoginPage");
   };
+
   return (
     <nav className="bg-white dark:bg-gray-900 w-full border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap gap-5 items-center justify-between mx-auto p-4">
-        <Link
+        <NavLink
           to="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
@@ -21,18 +24,18 @@ export default function MyHeader() {
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             CarrySite
           </span>
-        </Link>
+        </NavLink>
 
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <Link to="/LoginPage">
+          <NavLink to="/LoginPage">
             <button
               type="button"
               className="btn btn-primary btn-outline rounded-2xl w-20"
             >
               Login
             </button>
-          </Link>
-          <Link to="/LoginPage">
+          </NavLink>
+          <NavLink to="/LoginPage">
             <button
               type="button"
               className="btn btn-error btn-outline rounded-2xl ml-2 w-20"
@@ -40,7 +43,7 @@ export default function MyHeader() {
             >
               Logout
             </button>
-          </Link>
+          </NavLink>
           <button
             onClick={() => setIsOpen(!isOpen)}
             type="button"
@@ -83,57 +86,64 @@ export default function MyHeader() {
           dark:border-gray-700 w-full"
           >
             <li>
-              <Link
+              <NavLink
                 to="/"
-                className="block py-2 px-3 text-blue-700 rounded-sm 
-                md:bg-transparent  md:p-0"
+                className={({ isActive }) =>
+                  `block py-2 px-3 rounded-sm md:bg-transparent md:p-0 ${
+                    isActive ? "text-blue-600 font-bold" : "hover:text-blue-500"
+                  }`
+                }
               >
                 Home
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
                 to="/ProductPage"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 
-                md:hover:bg-transparent md:hover:text-blue-700 md:p-0 
-                dark:text-white md:dark:hover:text-blue-500 
-                dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={({ isActive }) =>
+                  `block py-2 px-3 rounded-sm md:bg-transparent md:p-0 ${
+                    isActive ? "text-blue-600 font-bold" : "hover:text-blue-500"
+                  }`
+                }
               >
                 Products
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
                 to="/CartPage"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 
-                md:hover:bg-transparent md:hover:text-blue-700 md:p-0 
-                dark:text-white md:dark:hover:text-blue-500 
-                dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={({ isActive }) =>
+                  `block py-2 px-3 rounded-sm md:bg-transparent md:p-0 ${
+                    isActive ? "text-blue-600 font-bold" : "hover:text-blue-500"
+                  }`
+                }
               >
                 Cart
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
                 to="/Contact"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 
-                md:hover:bg-transparent md:hover:text-blue-700 md:p-0 
-                dark:text-white md:dark:hover:text-blue-500 
-                dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={({ isActive }) =>
+                  `block py-2 px-3 rounded-sm md:bg-transparent md:p-0 ${
+                    isActive ? "text-blue-600 font-bold" : "hover:text-blue-500"
+                  }`
+                }
               >
                 Contact
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
                 to="/Userspage"
-                className=" py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 
-                md:hover:bg-transparent md:hover:text-blue-700 md:p-0 
-                dark:text-white md:dark:hover:text-blue-500 
-                dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hidden md:block"
+                className={({ isActive }) =>
+                  `py-2 px-3 rounded-sm md:bg-transparent md:p-0 hidden md:block ${
+                    isActive ? "text-blue-600 font-bold" : "hover:text-blue-500"
+                  }`
+                }
               >
                 Users
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </div>
